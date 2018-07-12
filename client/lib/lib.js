259dbbe1ac20b8a06e2b66421e9f68f8d9e7eb34
Jude <jronyagz.ixi@gmail.com>
2018-07-12 14:56:58 +0800

    update s3 config

--- client/lib/lib.js
+++ client/lib/lib.js
@@ -9,8 +9,8 @@
 var web3 = new Web3(Web3.givenProvider || "ws://localhost:8546");
 
 S3.config = {
-	key: 'AKIAJQN6HWIUJ3ZPULUA',
-	secret: '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',
-	bucket: 's3-ubetcoin-user-signatures',
+	key: 'AKIAIOWDXS3PQFEOL37A',
+	secret: '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',
+	bucket: 's3-ztktoken-user-signatures',
 	region: 'us-east-1' // Only needed if not "us-east-1" or ""
 };
